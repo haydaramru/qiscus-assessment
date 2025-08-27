0173185dd01d6bba4cc4f5caf7e8b4755371d907
haydaramru <haydaramrurevanda@mail.ugm.ac.id>
2025-08-28 01:53:49 +0700

    feat: add media comment variants (image, video, pdf)

--- lib/types.ts
+++ lib/types.ts
@@ -11,12 +11,35 @@ export type Room = {
 	participant: Participant[]
 }
 
-export type Comment = {
+export type CommentBase = {
 	id: number
-	type: 'text'
-	message: string
 	sender: string
 }
+
+export type CommentText = CommentBase & {
+	type: 'text'
+	message: string
+}
+
+export type CommentImage = CommentBase & {
+	type: 'image'
+	url: string
+	caption?: string
+}
+
+export type CommentVideo = CommentBase & {
+	type: 'video'
+	url: string
+	caption?: string
+}
+
+export type CommentPdf = CommentBase & {
+	type: 'pdf'
+	url: string
+	filename: string
+}
+
+export type Comment = CommentText | CommentImage | CommentVideo | CommentPdf
 
 export type ChatItem = {
 	room: Room
