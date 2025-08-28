4639cc41ca35ad55518686f856aeb29127d3288e
haydaramru <haydaramrurevanda@mail.ugm.ac.id>
2025-08-28 13:04:34 +0700

    style: enhance customer list component layout and styling for improved user experience

--- components/shared/customer-list/customerList.tsx
+++ components/shared/customer-list/customerList.tsx
@@ -14,15 +14,22 @@ const ItemList = ({ children, title, action: Action }: Props) => {
     const {isActive} = useRoomChat()
 
     return (
-        <Card className={cn("hidden h-full w-full lg:flex-none lg:w-md p-2", {
-            "block": !isActive,
-            "lg:block": isActive
-        })}>
-            <div className="mb-4 flex items-center justify-between">
-                <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
+        <Card
+            className={cn(
+                "hidden h-full w-full lg:flex-none lg:w-md p-0 rounded-none",
+                {
+                    "block": !isActive,
+                    "lg:block": isActive,
+                }
+            )}
+        >
+            <div className="flex items-center justify-between px-3 py-2 bg-primary/50 border-b">
+                <h1 className="text-2xl p-2 font-semibold tracking-tight">{title}</h1>
                 {Action ? Action : null}
             </div>
-            <div className="w-full h-full flex flex-col items-center justify-start gap-2">{children}</div>
+            <div className="w-full h-full overflow-y-auto no-scrollbar flex flex-col items-stretch justify-start pb-16 lg:pb-0">
+                {children}
+            </div>
         </Card>
     )
 }
